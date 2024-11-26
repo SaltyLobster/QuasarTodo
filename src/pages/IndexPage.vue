0442ef9a763a0c9ec5507e6b0b279fb66bb82cd1
SaltyLobster <megustalobster@gmail.com>
2024-11-26 05:58:46 +0100

    Finished with the basic version

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,113 @@
-<template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
-</template>
-
 <script setup>
-defineOptions({
-  name: 'IndexPage'
+import { ref, computed } from 'vue'
+
+
+class TaskList {
+  constructor(name) {
+    this.name = name
+    this.tasks = ref([])
+    this.idCounter = 0
+  }
+}
+
+
+let id = 0
+
+const showCompleted = ref(true)
+const taskText = ref('')
+const taskList = ref([
+  {
+    id: id++,
+    task: "Learn JS",
+    isCompleted: false
+  },
+  {
+    id: id++,
+    task: "Learn Quasar",
+    isCompleted: false
+  },
+  {
+    id: id++,
+    task: "Learn Vue",
+    isCompleted: true
+  },
+])
+
+const taskLists = ref([
+  new TaskList("Work"),
+  new TaskList("Personal"),
+  new TaskList("Hobbies"),
+]);
+
+const filteredTasks = computed(() => {
+  return showCompleted.value
+    ? taskList.value
+    : taskList.value.filter(task => !task.isCompleted);
 });
+
+
+ function addTask() {
+  if (taskText.value.trim() !== '') {
+    taskList.value.push({
+      id: id++,
+      task: taskText.value,
+      isCompleted: false
+        });
+        taskText.value = ''
+      }
+    }
+    function removeTask(taskId) {
+      taskList.value = taskList.value.filter(task => task.id != taskId)
+    }
+    function toggleCompleted(task) {
+      task.isCompleted = !task.isCompleted
+    }
+    function toggleShowCompleted() {
+      showCompleted.value = !showCompleted.value
+    }
+
+
 </script>
+
+<template>
+  <q-page class="body">
+    <div class="top-container">
+      <div class="form__group field">
+        <q-input
+          v-model="taskText"
+          @keyup.enter="addTask"
+          type="input"
+          placeholder="Start typing a task"
+          name="name"
+          id='name'
+          filled
+          />
+      </div>
+        <q-list class="task-list" separator>
+          <q-item
+          v-for="task in filteredTasks"
+          clickable
+          :key="task.id"
+          :class="{ completed: task.isCompleted }"
+          @click="toggleCompleted(task)">
+            <span>{{ task.task }}</span>
+            <q-btn
+              flat
+              dense
+              icon="close"
+              color="warning"
+              @click.stop="removeTask(task.id)"
+              />
+          </q-item>
+        </q-list>
+        <q-btn class="button" @click="toggleShowCompleted">
+          <span v-if="showCompleted==true">Hide completed</span>
+          <span v-else>Show completed</span>
+        </q-btn>
+      </div>
+  </q-page>
+</template>input
+
+<style scoped>
+
+</style>
